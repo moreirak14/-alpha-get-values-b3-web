b4b4fa9af2f731650dff6b27236829aab09b6d86
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-08-22 18:43:24 -0300

    feat: register new routes in App

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import HomePage from "containers/HomePage";
+import DashboardPage from "containers/DashboardPage";
+import LoginPage from "containers/LoginPage";
+import RegisterPage from "containers/RegisterPage";
+
+
 const App = () => (
-  <div>
-    <h1>My App</h1>
-  </div>
+  <Router>
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/dashboard' element={<DashboardPage />} />
+      <Route path='/login' element={<LoginPage />} />
+      <Route path='/register' element={<RegisterPage />} />
+    </Routes>
+  </Router>
 );
 
 export default App;
