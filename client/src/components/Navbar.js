ae3da8cc0bb68a1bb7fa22d6bb24c351936d4e3a
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-08-22 19:41:38 -0300

    feat: created Navbar with links

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,77 @@
+import { Link, NavLink } from "react-router-dom";
+
+const Navbar = () => {
+
+    const authLinks = (
+        <>
+            <li className="nav-item">
+                <NavLink className="nav-link" to="/dashboard">
+                    Dashboard
+                </NavLink>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#!">Logout</a>
+            </li>
+            {/* <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    My Actions
+                </a>
+                <ul className="dropdown-menu">
+                    <li><a className="dropdown-item" href="#">PETR4</a></li>
+                    <li><a className="dropdown-item" href="#">ITSA4</a></li>
+                    <li><a className="dropdown-item" href="#">MGLU3</a></li>
+                </ul>
+            </li> */}
+        </>
+    );
+
+    const guestLinks = (
+        <>
+            <li className="nav-item">
+                <NavLink className="nav-link" to="/login">
+                    Login
+                </NavLink>
+            </li>
+            <li className="nav-item">
+                <NavLink className="nav-link" to="/register">
+                    Register
+                </NavLink>
+            </li>
+        </>
+    );
+
+    return (
+        <nav className="navbar navbar-expand-lg bg-light">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/">
+                    Alpha Invest
+                </Link>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavDropdown"
+                    aria-controls="navbarNavDropdown"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/">
+                                Home
+                            </NavLink>
+                        </li>
+
+                        {authLinks}
+                        {guestLinks}
+                        {/* {isAuthenticated ? authLinks : guestLinks} */}
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
