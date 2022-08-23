400c6d0a9957dad855778692c8cfaed460765eea
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-08-23 18:21:27 -0300

    feat: adjusting login page

--- client/src/containers/LoginPage.js
+++ client/src/containers/LoginPage.js
@@ -1,19 +1,68 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { resetRegistered } from "features/user_auth";
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { resetRegistered, login } from "features/user_auth";
 import Layout from "components/Layout";
 
 
 const LoginPage = () => {
     const dispatch = useDispatch();
+    const { loading } = useSelector(state => state.user);
+
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    });
 
     useEffect(() => {
         dispatch(resetRegistered());
     }, []);
 
+    const { email, password } = formData;
+
+    const onChange = e => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const onSubmit = e => {
+        e.preventDefault();
+
+        dispatch(login({ email, password }));
+    };
+
     return (
         <Layout title='Alpha Invest | Login' content='Login Page'>
-            <h1>Login</h1>
+            <h1>Log into your Account</h1>
+            <form className="mt-5" onSubmit={onSubmit}>
+                <div className="form-group">
+                    <label className="form-label" htmlFor="first_name">Email</label>
+                    <input
+                        className="form-control"
+                        type='email'
+                        name='email'
+                        onChange={onChange}
+                        value={email}
+                        required
+                    />
+                </div>
+                <div className="form-group mt-3">
+                    <label className="form-label" htmlFor="first_name">Password</label>
+                    <input
+                        className="form-control"
+                        type='password'
+                        name='password'
+                        onChange={onChange}
+                        value={password}
+                        required
+                    />
+                </div>
+                {loading ? (
+                    <div className="spinner-border text-primary" role="status">
+                        <span className="visually-hidden">Loading...</span>
+                    </div>
+                ) : (
+                    <button className="btn btn-primary mt-4">Login</button>
+                )}
+            </form>
         </Layout>
     )
 }
