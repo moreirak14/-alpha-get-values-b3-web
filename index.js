e30c270811d1a017e994d041a0d584a3c0d70d6e
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-08-23 18:19:50 -0300

    feat: register router login and me

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const path = require('path');
 
 require('dotenv').config();
 
+const loginRouter = require('./routes/auth/login');
+const meRouter = require('./routes/auth/me');
 const registerRouter = require('./routes/auth/register');
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
+
+app.use(loginRouter);
+app.use(meRouter);
 app.use(registerRouter);
+
 app.use(express.static('client/build'));
 app.get('*', (req, res) => {
     const myPath = path.resolve(__dirname, 'client', 'build', 'index.html');
