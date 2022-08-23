6bee99e7653b096191a2bfe252756772879f09bb
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-08-23 18:21:03 -0300

    feat: added get user, login and new cases

--- client/src/features/user_auth.js
+++ client/src/features/user_auth.js
@@ -28,10 +28,66 @@ export const register = createAsyncThunk(
             } else {
                 return thunkAPI.rejectWithValue(data);
             }
-        } catch (error) {
-            return thunkAPI.rejectWithValue(error.response.data);
+        } catch (err) {
+            return thunkAPI.rejectWithValue(err.response.data);
         }
      }
+);
+
+const getUser = createAsyncThunk('users/me', async (_, thunkAPI) => {
+	try {
+		const res = await fetch('/api/users/me', {
+			method: 'GET',
+			headers: {
+				Accept: 'application/json',
+			},
+		});
+
+		const data = await res.json();
+
+		if (res.status === 200) {
+			return data;
+		} else {
+			return thunkAPI.rejectWithValue(data);
+		}
+	} catch (err) {
+		return thunkAPI.rejectWithValue(err.response.data);
+	}
+});
+
+export const login = createAsyncThunk(
+	'users/login',
+	async ({ email, password }, thunkAPI) => {
+		const body = JSON.stringify({
+			email,
+			password,
+		});
+
+		try {
+			const res = await fetch('/api/users/login', {
+				method: 'POST',
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/json',
+				},
+				body: body,
+			});
+
+			const data = await res.json();
+
+			if (res.status === 200) {
+				const { dispatch } = thunkAPI;
+
+				dispatch(getUser());
+
+				return data;
+			} else {
+				return thunkAPI.rejectWithValue(data);
+			}
+		} catch (err) {
+			return thunkAPI.rejectWithValue(err.response.data);
+		}
+	}
 );
 
 const initialState = {
@@ -61,7 +117,27 @@ const userSlice = createSlice({
             .addCase(register.rejected, state => {
                 state.loading = false;
             })
-    }
+            .addCase(login.pending, state => {
+                state.loading = true;
+            })
+            .addCase(login.fulfilled, state => {
+                state.loading = false;
+                state.isAuthenticated = true;
+            })
+            .addCase(login.rejected, state => {
+                state.loading = false;
+            })
+            .addCase(getUser.pending, state => {
+                state.loading = true;
+            })
+            .addCase(getUser.fulfilled, (state, action) => {
+                state.loading = false;
+                state.user = action.payload;
+            })
+            .addCase(getUser.rejected, state => {
+                state.loading = false;
+            });
+    },
 });
 
 export const { resetRegistered } = userSlice.actions;
