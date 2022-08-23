0f82aa33ac3a795b3e57478c29666b41f267a098
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-08-23 16:57:04 -0300

    feat: redirect login page after registration

--- client/src/containers/LoginPage.js
+++ client/src/containers/LoginPage.js
@@ -1,7 +1,16 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { resetRegistered } from "features/user_auth";
 import Layout from "components/Layout";
 
 
 const LoginPage = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(resetRegistered());
+    }, []);
+
     return (
         <Layout title='Alpha Invest | Login' content='Login Page'>
             <h1>Login</h1>
