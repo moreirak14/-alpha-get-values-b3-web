1a7b7285e700d2bd1be10ccbdb36f62093510d31
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-08-23 20:12:32 -0300

    feat: added fake list-group with actions

--- client/src/containers/DashboardPage.js
+++ client/src/containers/DashboardPage.js
@@ -1,12 +1,38 @@
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
 import Layout from "components/Layout";
 
 
 const DashboardPage = () => {
+    const { isAuthenticated, user, loading } = useSelector(state => state.user);
+
+    if (!isAuthenticated && !loading && user === null)
+        return <Navigate to='/login' />;
+
     return (
         <Layout title='Alpha Invest | Dashboard' content='Dashboard Page'>
-            <h1>Dashboard</h1>
+            {loading || user === null ? (
+                <div className="spinner-border text-primary" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </div>
+            ) : (
+                <>
+					<h1 className='mb-5'>Dashboard</h1>
+					<p>User Details</p>
+					<ul>
+                        <li>Name: {user.first_name}  {user.last_name}</li>
+						<li>Email: {user.email}</li>
+                    </ul>
+                    <p>My Actions</p>
+                    <div className="list-group">
+                        <a href="#!" className="list-group-item list-group-item-action">PETR4</a>
+                        <a href="#!" className="list-group-item list-group-item-action">ITSA4</a>
+                        <a href="#!" className="list-group-item list-group-item-action">MGLU3</a>
+                    </div>
+				</>
+            )}
         </Layout>
-    )
-}
+    );
+};
 
 export default DashboardPage;
